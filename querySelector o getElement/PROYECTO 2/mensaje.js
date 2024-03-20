e94e2f755dc878244925a8c8d2c6b0d375dba599
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-03-20 18:40:16 -0500

    mensaje/teclado codigo

--- querySelector o getElement/PROYECTO 2/mensaje.js
+++ querySelector o getElement/PROYECTO 2/mensaje.js
@@ -1,14 +1,28 @@
 let mensaje = ""
-
+let usuario = ""
 function inicio() {
-    document.querySelector("#escribir").value
+    document.querySelector("#escribir").onkeydown = teclado
     document.querySelector("#mandarMensaje").onclick = mandarMensaje
-    
+    document.querySelector("#usuario").value
     console.log("listo js")
 }
 function mandarMensaje() {
     mensaje = document.querySelector("#escribir").value;
-    document.querySelector("#mensajes").innerHTML += `${mensaje} <br>`
+    usuario = document.querySelector("#usuario").value
+    document.querySelector("#mensajes").innerHTML += `>${usuario} escribio <br> ${mensaje} <br>`
+
+    document.querySelector("#escribir").value = ""
+    document.querySelector("#escribir").focus()
+
+}
+
+
+
+function teclado(e) {
+    let codigo_tecla = e.keyCode
+    if(codigo_tecla == 13){
+        mandarMensaje()
+    }
 }
 window.onload = inicio
 console.log(mensaje)
