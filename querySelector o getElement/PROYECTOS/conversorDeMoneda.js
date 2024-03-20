5949d8e12ddf8b3e933f723f76a0b4bffeb2b57b
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-03-20 16:12:58 -0500

    conversorDeMoneda/se puede agregar dinero y cambio

--- querySelector o getElement/PROYECTOS/conversorDeMoneda.js
+++ querySelector o getElement/PROYECTOS/conversorDeMoneda.js
@@ -7,15 +7,18 @@ function convertir(a, b) {
 }
 
 function iniciar_convercion() {
+    cambio = document.querySelector("#cambio").value
     dinero = document.querySelector("#dinero").value;
     //dinero = document.querySelector("dinero").value; = agarra el valor escrito en el input y lo agrega a la variable dinero
    recibo=convertir(dinero,cambio)
     console.log(recibo + "col este es el resulado de cambiar peso colombiano a euro")
-    document.querySelector("#conversines").innerHTML += `${recibo} es el recibo <br>`
+    document.querySelector("#conversines").innerHTML += `${dinero} euros son ${recibo} pesos colombianos <br>`
 }
 
 window.onload = inicio;
 
 function inicio() {
+
     document.querySelector("#convertir").onclick = iniciar_convercion
+    document.querySelector("#cambio").value = cambio
 }
