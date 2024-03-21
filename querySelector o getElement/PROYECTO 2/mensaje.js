e2c2edb4a7959551a2107e26e1ec71c514251155
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-03-20 19:27:11 -0500

    mensaje/color de usuario en el div de mensajes

--- querySelector o getElement/PROYECTO 2/mensaje.js
+++ querySelector o getElement/PROYECTO 2/mensaje.js
@@ -4,12 +4,13 @@ function inicio() {
     document.querySelector("#escribir").onkeydown = teclado
     document.querySelector("#mandarMensaje").onclick = mandarMensaje
     document.querySelector("#usuario").value
+    document.querySelector("nombreDeUsuario")
     console.log("listo js")
 }
 function mandarMensaje() {
     mensaje = document.querySelector("#escribir").value;
     usuario = document.querySelector("#usuario").value
-    document.querySelector("#mensajes").innerHTML += `>${usuario} escribio <br> ${mensaje} <br>`
+    document.querySelector("#mensajes").innerHTML += `><span>${usuario} </span> escribio <br> ${mensaje} <br>`
 
     document.querySelector("#escribir").value = ""
     document.querySelector("#escribir").focus()
