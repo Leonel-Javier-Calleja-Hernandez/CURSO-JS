719fe2ef01ab22a241f458031974010f5ceffcbb
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-04-03 19:23:55 -0500

    OPERADORES Y EXPRECIONES/DE COMPARACION

---
+++ OPERADORES Y EXPRESIONES/DE COMPARACION/ejercicios.js
@@ -0,0 +1,21 @@
+var a = 10
+var b = "10"
+var aIgualBvalor = a==b
+var aIgualBvalorYTipo = a===b
+var aDiferenteBValor = a!=b
+var aDiferenteBValorYTipo = a!==b
+alert("a es igual a b por su valor " + aIgualBvalor )
+alert("a es igual a b su valor y tipo de dato " + aIgualBvalorYTipo )
+alert("a es diferento por su valor que b " + aDiferenteBValor)
+alert("a es diferente de b por su valor y tipo de dato " + aDiferenteBValorYTipo)
+alert("a es mayor que b " + a>b)
+alert("a es menor que b " + a<b)
+alert("a es mayor o igua que b" + a>=b)
+alert("a es menor o igual que b" + a<=b)
+
+var numero1 = prompt("numero 1")
+var numero2 = prompt("numero 2")
+alert("numero1==numero2 "+(numero1==numero2))
+alert("numero1===numero2 "+(numero1===numero2))
+alert("numero1 > numero2 "+(numero1 > numero2))
+alert("numero1 < numero2 "+(numero1 < numero2))
---
+++ OPERADORES Y EXPRESIONES/INCREMENTO/ejercicios.js
@@ -0,0 +1,12 @@
+var numero1 = 10
+// var numero2 = ++numero1 * 2
+// alert(numero1)
+// alert(numero2)
+// var numero3 = --numero1 * 2
+// alert(numero2)
+alert(`el numero es ${numero1}`)
+var numero4 =numero1++ * 2
+alert(`el numero es ${numero1}`)
+alert(numero4)
+alert(numero1 * 2)
+alert(numero1 * 2)
