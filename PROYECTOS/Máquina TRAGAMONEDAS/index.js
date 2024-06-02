b5e48731fe3f5121a8eeed9ce9accba38b9bf07e
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-06-02 14:42:55 -0500

    PROYECTOS/Maquina TRAGAMONEDAS/index.js/inicio

---
+++ PROYECTOS/Máquina TRAGAMONEDAS/index.js
@@ -0,0 +1,33 @@
+window.onload=inicio 
+
+function inicio() {
+    
+}
+
+function TirarInicio() {
+    
+}
+function lanzar_uno() {
+    
+}
+function escojer_numero() {
+    
+}
+function mostrar_imagen() {
+    
+}
+function comparar() {
+    
+}
+function actualizar() {
+    
+}
+function mostrar_mensaje() {
+    
+}
+function cerrar() {
+    
+}
+function sonidos() {
+    
+}
