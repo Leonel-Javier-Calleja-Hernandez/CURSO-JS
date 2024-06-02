e08876e2a17cbaaf9532ba0500f4c2fbb8da5c27
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-06-02 15:15:52 -0500

    PROYECTOS/Maquina TRAGAMONEDAS/index.js/haciendo funcionar el boton tirar /inicio

--- PROYECTOS/Máquina TRAGAMONEDAS/index.js
+++ PROYECTOS/Máquina TRAGAMONEDAS/index.js
@@ -1,17 +1,27 @@
 window.onload=inicio 
+var credito=Math.floor(Math.random()*4)+9
+var imagenes=["bombum.png","espejo.png","gorra.png","llavero.png","sacapuntas.png","tenedor.png"]
+var premios=[3,2,3,2,3,2]
+var numeros_actualizar=[]
+
 
 function inicio() {
-    
+    document.getElementById("tirar").onclick=Tirar_Inicio
 }
 
-function TirarInicio() {
-    
+function Tirar_Inicio() {
+    numeros_actualizar=[]
+    for (let k = 0; k < document.getElementsByClassName("botonAbajo").length; k++) {
+        numeros_actualizar.push(escojer_numero())
+    }
+    alert(numeros_actualizar)
 }
 function lanzar_uno() {
     
 }
 function escojer_numero() {
-    
+    var azar=Math.floor(Math.random()*imagenes.length)
+    return azar
 }
 function mostrar_imagen() {
     
