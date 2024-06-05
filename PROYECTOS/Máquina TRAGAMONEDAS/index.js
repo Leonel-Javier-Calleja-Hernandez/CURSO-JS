a6871012aeb9a49a615b482c1283257dd43cf24e
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-06-05 14:10:42 -0500

    PROYECTOS/Maquina TRAGAMONEDAS/index.js/solucion de errores en mostrar las imagenes

--- PROYECTOS/Máquina TRAGAMONEDAS/index.js
+++ PROYECTOS/Máquina TRAGAMONEDAS/index.js
@@ -1,8 +1,8 @@
 window.onload=inicio 
 var credito=Math.floor(Math.random()*4)+9
-var imagenes=["bombum.png","espejo.png","gorra.png","llavero.png","sacapuntas.png","tenedor.png"]
+var imagenes=["bombum.jpeg","espejo.jpeg","gorra.jpeg","llavero.jpeg","sacapunta.jpeg","tenedor.jpeg"]
 var premios=[3,2,3,2,3,2]
-var numeros_actualizar=[]
+var numeros_actuales=[]
 
 
 function inicio() {
@@ -10,21 +10,24 @@ function inicio() {
 }
 
 function Tirar_Inicio() {
-    numeros_actualizar=[]
+    numeros_actuales=[]
     for (let k = 0; k < document.getElementsByClassName("botonAbajo").length; k++) {
-        numeros_actualizar.push(escojer_numero())
+        // pienza un numero y lo introduce dentro de el array numeros_actulales
+        numeros_actuales.push(escojer_numero())
+        mostrar_imagen(k,numeros_actuales[k])
     }
-    alert(numeros_actualizar)
 }
 function lanzar_uno() {
     
 }
 function escojer_numero() {
+    // pensara un numero entre la cantidad de elementos que hay en el array imagenes y lo devuelve con return
     var azar=Math.floor(Math.random()*imagenes.length)
     return azar
 }
-function mostrar_imagen() {
-    
+function mostrar_imagen(num,img) {
+    console.log(num,img)
+    document.getElementsByClassName("imagen")[num].getElementsByTagName("img")[0].src="img/" + imagenes[img];
 }
 function comparar() {
     
