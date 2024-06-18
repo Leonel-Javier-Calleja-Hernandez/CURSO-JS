fc1577a868867d1bf32bc8d9089fb64e58991aa1
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-06-18 14:28:02 -0500

    PROYECTOS/Maquina TRAGAMONEDAS/index.js/el this esta funcionando y los creditos se muestran en pantalla como el dinero

--- PROYECTOS/Máquina TRAGAMONEDAS/index.js
+++ PROYECTOS/Máquina TRAGAMONEDAS/index.js
@@ -4,6 +4,7 @@ var imagenes=["bombum.jpeg","espejo.jpeg","gorra.jpeg","llavero.jpeg","sacapunta
 var premios=[3,2,3,2,3,2];
 var numeros_actuales=[];
 var au;
+var activos=false
 
 function inicio() {
     document.getElementById("tirar").onclick=Tirar_Inicio
@@ -13,9 +14,13 @@ function inicio() {
       document.getElementsByClassName("botonAbajo")[k].onclick=lanzar_uno
         
     }
+    actualizar();
 }
 
 function Tirar_Inicio() {
+    if (credito>0) {
+        sonidos("salto-mario.mp3");
+        activos=true;
     numeros_actuales=[]
     // document.getElementsByClassName("botonAbajo").length lo que hace es buscar la cantidad de veces que la class botonAbajo esta en
     for (let k = 0; k < document.getElementsByClassName("botonAbajo").length; k++) {
@@ -25,13 +30,28 @@ function Tirar_Inicio() {
     }
     comparar()
 }
-function lanzar_uno() {
-    this.style.opacity="0.2";
 }
-function escojer_numero() {
+function lanzar_uno() {
+    sonidos("salto-mario.mp3");
+    if (credito>0 && activos==true) {
+    let hijos = this.parentNode.parentNode.children
+    for (let k = 0; k < hijos.length; k++) {
+        if(this.parentNode==hijos[k]){
+            numeros_actuales[k]=escojer_numero(numeros_actuales[k]);
+            mostrar_imagen(k,numeros_actuales[k]);
+            comparar()
+            break;
+        }
+    }
+}
+}
+function escojer_numero(actual) {
+    do{
+        var azar=Math.floor(Math.random()*imagenes.length)
+    }while (azar==actual) {
+        return azar;
+    }
     // pensara un numero entre la cantidad de elementos que hay en el array imagenes y lo devuelve con return
-    var azar=Math.floor(Math.random()*imagenes.length)
-    return azar
 }
 function mostrar_imagen(num,img) {
     console.log(num,img)
@@ -42,6 +62,7 @@ function comparar() {
     if(numeros_actuales[0]==numeros_actuales[1]&&
         numeros_actuales[1]==numeros_actuales[2]
     ){
+        activos=false
         let p = premios[numeros_actuales[0]]
         let mensaje=`HAS GANADO ${p} MONEDAS <div>`;
         for (let k = 0; k < p; k++) {
@@ -51,10 +72,23 @@ function comparar() {
         mensaje+="</div>"
         mostrar_mensaje(mensaje)
         sonidos("victory_sJDDywi.mp3")
+        credito+=premios[numeros_actuales[0]];
+
     }
+    credito--;
+    actualizar()
 }
 function actualizar() {
-    
+    document.getElementById("monedas").innerHTML="";
+    document.getElementById("dinero").innerHTML=credito
+    for (let k = 1; k < credito; k++) {
+       document.getElementById("monedas").innerHTML+=`
+       <img src="./img/monedas.png" alt="">`
+        
+    }
+    if (credito<1) {
+        mostrar_mensaje(`<b>GAME OVER</b><div class="Mensaje_Perdiste">No te queda dinero :/</div>`)
+    }
 }
 function mostrar_mensaje(m) {
     document.getElementById("velo").style.display="block";
